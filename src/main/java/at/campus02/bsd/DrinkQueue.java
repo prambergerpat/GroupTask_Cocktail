5a6523216921cd62f25d8bf835bcdeea098735b0
Samuel Ulz <s54090@edu.campus02.at>
2025-05-14 11:40:20 +0200

    Added IQueue interface

--- src/main/java/at/campus02/bsd/DrinkQueue.java
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -1,4 +1,29 @@
 package at.campus02.bsd;
 
-public class DrinkQueue {
+public class DrinkQueue implements IQueue{
+
+    @Override
+    public boolean offer(String obj) {
+        return false;
+    }
+
+    @Override
+    public String poll() {
+        return "";
+    }
+
+    @Override
+    public String remove() {
+        return "";
+    }
+
+    @Override
+    public String peek() {
+        return "";
+    }
+
+    @Override
+    public String element() {
+        return "";
+    }
 }
