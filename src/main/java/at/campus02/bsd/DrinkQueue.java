557271b4d35f24315ba78fbd79112c998c18879f
Samuel Ulz <s54090@edu.campus02.at>
2025-05-29 17:07:28 +0200

    Add comments

--- src/main/java/at/campus02/bsd/DrinkQueue.java
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -1,17 +1,24 @@
 package at.campus02.bsd;
 
-import at.campus02.bsd.Drink;
-import at.campus02.bsd.SimpleDrink;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * DrinkQueue is a simple Queue implementation for objects using the Drink interface
+ */
 public class DrinkQueue{
 
-    List<Drink> elements = new ArrayList<Drink>();
+    // List of Drinks
+    List<Drink> elements = new ArrayList<>();
 
-    public boolean offer(SimpleDrink drink) {
+    /**
+     * Adds a new drink to the queue
+     *
+     * @param drink new drink to add
+     * @return true if drink was added, otherwise false
+     */
+    public boolean offer(Drink drink) {
         if (drink == null) {
             return false;
         }
@@ -19,6 +26,11 @@ public class DrinkQueue{
         return true;
     }
 
+    /**
+     * Removes and returns the first drink from the queue
+     *
+     * @return the first drink in the queue, otherwise null if queue is empty
+     */
     public Drink poll() {
         if (elements.isEmpty()) {
             return null;
@@ -26,21 +38,35 @@ public class DrinkQueue{
         return elements.removeFirst();
     }
 
+    /**
+     * Removes and returns the first drink from the queue
+     *
+     * @return the first drink in the queue
+     * @throws NoSuchElementException if queue is empty
+     */
     public Drink remove() {
         if (elements.isEmpty()) {
             throw new NoSuchElementException("Queue is empty");
         }
 
-        Drink firstDrink = elements.getFirst();
-        elements.removeFirst();
-
-        return firstDrink;
+        return elements.removeFirst();
     }
 
+    /**
+     * Returns the first drink from the queue without removing it
+     *
+     * @return the first drink in the queue, otherwise null if queue is empty
+     */
     public Drink peek() {
         return elements.isEmpty() ? null : elements.getFirst();
     }
 
+    /**
+     * Returns the first drink from the queue without removing it
+     *
+     * @return the first drink in the queue
+     * @throws NoSuchElementException if queue is empty
+     */
     public Drink element() {
         return elements.getFirst();
     }
