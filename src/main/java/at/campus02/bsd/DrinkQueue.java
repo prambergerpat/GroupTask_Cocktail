d24109df32c4da28df75ed2d18af92cbb7646c3c
Samuel Ulz <s54090@edu.campus02.at>
2025-05-29 15:43:50 +0200

    Add DrinkQueue class

---
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -0,0 +1,29 @@
+package at.campus02.bsd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrinkQueue{
+
+    List<Drink> elements = new ArrayList<Drink>();
+
+    public boolean offer(String obj) {
+        return false;
+    }
+
+    public String poll() {
+        return "";
+    }
+
+    public String remove() {
+        return "";
+    }
+
+    public String peek() {
+        return "";
+    }
+
+    public String element() {
+        return "";
+    }
+}
